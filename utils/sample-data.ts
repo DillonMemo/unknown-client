bf6aed32590bca3364307dab825315c46a046e6b
Dillon Jang <arta1069@gmail.com>
2024-01-01 00:05:04 +0900

    second

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -1,9 +1,9 @@
-import { User } from '../interfaces'
+import { NewYear } from "../interfaces";
 
 /** Dummy user data. */
-export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
-]
+export const sampleUserData: NewYear[] = [
+    { id: 101, name: "Alice", text: "" },
+    { id: 102, name: "Bob", text: "" },
+    { id: 103, name: "Caroline", text: "" },
+    { id: 104, name: "Dave", text: "" },
+];
